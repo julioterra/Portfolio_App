1aa94c63efa6937c807364c1d3e7bca456b6b0c6
Julio Terra <julioterra@gmail.com>
2011-10-18 22:22:08 -0400

    finished playing around with static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  def title
+    base_title = "Welcome to my Rails app"
+    if @title
+      "#{base_title}, this is the page: #{@title}"
+    else
+      "#{base_title}, this is a title-less page"
+    end    
+  end
+  
 end
