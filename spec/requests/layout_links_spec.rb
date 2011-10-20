db9930c03f49137bb1c0017a34823c5cf666523b
Julio Terra <julioterra@gmail.com>
2011-10-20 01:56:14 -0400

    added named routes for all existing pages and used integration tests to make sure all is working properly.

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -17,4 +17,9 @@ describe 'LayoutLinks' do
     response.should have_selector 'title', content: "About"
   end
   
+  it "should have a Sign up page at '/signup'" do
+    get '/signup'
+    response.should have_selector 'title', content: "Sign Up"
+  end
+  
 end
